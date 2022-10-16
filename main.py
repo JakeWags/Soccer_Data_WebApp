3a965c0b1884b4a065dc62e53b45e570e75792cb
Jake Wagoner <jakewagoneredu@gmail.com>
2022-10-16 10:30:09 -0600

    Added all method stubs for API with routes

--- main.py
+++ main.py
@@ -3,10 +3,36 @@ from flask import Flask, render_template, url_for, json
 
 app = Flask(__name__)
 
-json_data = os.path.join(app.static_folder, 'data', 'soccer_small.json')
+json_filename = os.path.join(app.static_folder, 'data', 'soccer_small.json')
+
+with open(json_filename, 'r', encoding='utf8') as json_output:  # file not using CPC1252 encoding, pyflask standard 
+    json_data = app.json.load(json_output)
 
 @app.route('/')
 def showjson():
-    with open(json_data, 'r', encoding='utf8') as json_output:  # file not using CPC1252 encoding, pyflask standard 
-        data = app.json.load(json_output)
-    return render_template('output.html', data=data)
+    return json_data
+
+# returns a player and all the player attributes
+@app.route('/players/<name>')
+def show_player(name):
+    return None
+
+# returns all players and their attributes
+@app.route('/players')
+def show_all_players():
+    return None
+
+# returns all countries with a list of players from those countries
+@app.route('/countries')
+def show_countries():
+    return None
+
+# returns all clubs with a list of players playing for those clubs
+@app.route('/clubs')
+def show_clubs():
+    return None
+
+# returns a list of all attribute names
+@app.route('/attributes')
+def show_attributes():
+    return None
