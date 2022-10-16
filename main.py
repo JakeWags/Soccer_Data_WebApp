d8ec19afa13d6a2ceaa4c3950fd220a2feb0a1d2
Jake Wagoner <jakewagoneredu@gmail.com>
2022-10-16 10:50:15 -0600

    /players/ and /players/<name> functional

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 json_filename = os.path.join(app.static_folder, 'data', 'soccer_small.json')
 
 with open(json_filename, 'r', encoding='utf8') as json_output:  # file not using CPC1252 encoding, pyflask standard 
-    json_data = app.json.load(json_output)
+    json_data = json.load(json_output)
+
 
 @app.route('/')
 def showjson():
@@ -15,24 +16,27 @@ def showjson():
 # returns a player and all the player attributes
 @app.route('/players/<name>')
 def show_player(name):
-    return None
+    for item in json_data:
+        if item["Name"] == name:
+            print(item["Name"])
+            return item
 
 # returns all players and their attributes
-@app.route('/players')
+@app.route('/players/')
 def show_all_players():
-    return None
+    return json_data
 
 # returns all countries with a list of players from those countries
-@app.route('/countries')
+@app.route('/countries/')
 def show_countries():
     return None
 
 # returns all clubs with a list of players playing for those clubs
-@app.route('/clubs')
+@app.route('/clubs/')
 def show_clubs():
     return None
 
 # returns a list of all attribute names
-@app.route('/attributes')
+@app.route('/attributes/')
 def show_attributes():
     return None
